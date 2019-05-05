50463c598c99e300d8156343cb5d6584886c2ab7
vocalnight <vocalebooks@gmail.com>
2019-05-04 21:40:31 -0300

    Fixed a bug when searching for a item in the datacenter.
    If a server didn't have the item, it would still be emitted in the zip, causing the wrong server to be assigned to the price.

--- src/commands/lowprice.ts
+++ src/commands/lowprice.ts
@@ -1,17 +1,17 @@
-import { Command } from "./command";
-import { CommandContext } from "../context/command_context";
+import {Command} from "./command";
+import {CommandContext} from "../context/command_context";
 
-import { concatMap, filter, map, reduce, switchMap, tap } from "rxjs/operators";
-import { Observable, Subscription, zip } from "rxjs";
-import { Results } from "../xiavpiclasses/Results";
-import { flatMap } from "rxjs/internal/operators";
-import { fromArray } from "rxjs/internal/observable/fromArray";
+import {filter, map, reduce, switchMap, tap} from "rxjs/operators";
+import {Observable, Subscription, zip} from "rxjs";
+import {Results} from "../xiavpiclasses/Results";
+import {flatMap} from "rxjs/internal/operators";
+import {fromArray} from "rxjs/internal/observable/fromArray";
 
-import { Searchparams } from "../xiavpiclasses/searchparams";
-import { MarketInformation } from "../xiavpiclasses/marketInformation";
-import { MarketPrices } from "../xiavpiclasses/marketPrices";
-import { Utils } from "../xiavpiclasses/utils";
-import { fromPromise } from "rxjs/internal-compatibility";
+import {Searchparams} from "../xiavpiclasses/searchparams";
+import {MarketInformation} from "../xiavpiclasses/marketInformation";
+import {MarketPrices} from "../xiavpiclasses/marketPrices";
+import {Utils} from "../xiavpiclasses/utils";
+import {fromPromise} from "rxjs/internal-compatibility";
 
 const XIVAPI = require('xivapi-js');
 
@@ -71,8 +71,8 @@ export class Lowprice extends Utils implements Command {
                             map(price => ({price, server})))
                 ))
             .subscribe((obj: { price: any, server: string }) => {
-                console.log("----");
-                console.log(obj);
+                    console.log("----");
+                    console.log(obj);
                     this.pushToArray(obj);
                 },
                 (error) => console.log(error),
@@ -96,35 +96,25 @@ export class Lowprice extends Utils implements Command {
             .subscribe((result) => {
                 this.servers$ = fromArray(Object.keys(result));
                 this.markets$ = fromArray(Object.values(result));
-                this.processPricesMultiple(userCommand);
+                this.joinServerPrices(userCommand);
             }));
     }
 
-    private processPricesMultiple(userCommand: CommandContext) {
+    private joinServerPrices(userCommand: CommandContext) {
 
         // Since the Object comes with the servers as keys,
         // i had to separate them from the body and use zip to add it to the correspondent price information
 
         this.subscription.add(
-            zip(this.servers$
-                .pipe(tap((data) => console.log(data))), this.markets$
-                .pipe(
-                    // Using concat to be ABSOLUTELY sure that the order is kept.
-                    concatMap((market: MarketInformation) => fromArray(market.Prices)
-                        .pipe(
-
-                            filter((prices: MarketPrices) => this.searchHQ ? prices.IsHQ : !prices.IsHQ),
-                            reduce((lowest: number, price: MarketPrices) =>
-                                price.PricePerUnit < lowest ? price.PricePerUnit : lowest)))
-                ))
-                .pipe(map(([server, price]) => {
-                    return ({server, price})
+            zip(this.servers$, this.markets$
+                .pipe(map((market: MarketInformation) => market.Prices)))
+                .pipe(map(([server, prices]) => {
+                    return ({server, prices})
                 }))
                 .subscribe(
-                    (obj: { server: string, price: any }) => {
-                        // I can only send the message to the chat when the observable completes,
-                        // so it's better to add everything to an array first and THEN print it
-                        this.pushToArray(obj);
+                    (obj: { server: string, prices: MarketPrices[] }) => {
+                        // Now that i have an object with the list of prices and the server, i can filter.
+                        this.processPrices(obj);
                     }, (error) => (console.log(error)),
                     () => {
                         this.printValues(userCommand);
@@ -132,6 +122,27 @@ export class Lowprice extends Utils implements Command {
                     }
                 )
         );
+    }
+
+    // The old method was getting lost when not all servers had the price, it would result in the wrong servers having the price.
+    // So i had to change to make sure that if the server dosn't have a listing, it never get's passed to the final response.
+    private processPrices(market: { server: string, prices: MarketPrices[] }): void {
+
+        const price: MarketPrices[] = market.prices.filter(price => this.searchHQ ? price.IsHQ : !price.IsHQ);
+        if (price && price.length) {
+            const lowestPrice = price.reduce((lowest: any, price: any) => {
+                if (price.PricePerUnit < lowest) {
+                    return price.PricePerUnit;
+                } else {
+                    return lowest;
+                }
+            });
+
+            // I can only send the message to the chat when the observable completes,
+            // so it's better to add everything to an array first and THEN print it
+
+            this.pushToArray({server: market.server, price: lowestPrice});
+        }
     }
 
     private printValues(userCommand: CommandContext): void {
